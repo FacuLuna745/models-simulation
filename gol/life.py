108e5f4c4812c56ccefa373b06b2e304c6bf8b60
faculuna745 <lunafacundo03@gmail.com>
2021-03-30 20:25:31 -0300

    Add counter to generation and box dead and life

--- gol/life.py
+++ gol/life.py
@@ -8,14 +8,14 @@ def read_matrix():
 
 
 def check_neighbor(matrix, x, y):
-    neighbor = matrix[x % 10][(y - 1) % 10] + \
-               matrix[x % 10][(y + 1) % 10] + \
-               matrix[(x - 1) % 10][(y - 1) % 10] + \
-               matrix[(x - 1) % 10][y % 10 % 10] + \
-               matrix[(x - 1) % 10][(y + 1) % 10] + \
-               matrix[(x + 1) % 10][(y - 1) % 10] + \
-               matrix[(x + 1) % 10][y % 10] + \
-               matrix[(x + 1) % 10][(y + 1) % 10]
+    neighbor = matrix[x % len(matrix)][(y - 1) % len(matrix)] + \
+               matrix[x % len(matrix)][(y + 1) % len(matrix)] + \
+               matrix[(x - 1) % len(matrix)][(y - 1) % len(matrix)] + \
+               matrix[(x - 1) % len(matrix)][y % len(matrix)] + \
+               matrix[(x - 1) % len(matrix)][(y + 1) % len(matrix)] + \
+               matrix[(x + 1) % len(matrix)][(y - 1) % len(matrix)] + \
+               matrix[(x + 1) % len(matrix)][y % len(matrix)] + \
+               matrix[(x + 1) % len(matrix)][(y + 1) % len(matrix)]
     return neighbor
 
 
@@ -31,11 +31,44 @@ def survival(matrix):
     return np.copy(matrix_aux)
 
 
+def check_life_dead(matrix):
+    life = 0
+    dead = 0
+    for x in range(len(matrix)):
+        for y in range(len(matrix)):
+            if matrix[x][y] == 0:
+                dead += 1
+            else:
+                life += 1
+    return life, dead
+
+
+def check_gen(counter_gen):
+    return counter_gen + 1
+
+
+def check_running_gen(matrix):
+    counter = 0
+    for x in range(len(matrix)):
+        for y in range(len(matrix)):
+            if matrix[x][y] == 0:
+                counter += 1
+    if counter == 100:
+        print("Se salio de la ejecución ya que no hay mas generaciones")
+        return False
+    else:
+        return True
+
+
 if __name__ == '__main__':
     run = True
     matrix = read_matrix()
+    gen = 0
     while run:
         matrix = survival(matrix)
-        print(matrix)
-        time.sleep(0.9)
+        check = check_life_dead(matrix)
+        gen = check_gen(gen)
+        print(f"{matrix} \nCeldas vivas: {check[0]} \nCeldas muertas: {check[1]} \nGeneración: {gen}")
+        time.sleep(1)
         os.system('clear')
+        run = check_running_gen(matrix)
