0f4c75ddf62dafcdb79302e6a09838e7bc32e621
faculuna745 <lunafacundo03@gmail.com>
2021-05-10 12:12:23 -0300

    Add readme and comment

---
+++ sympy/README.md
@@ -0,0 +1,11 @@
+# Ecuaciones Diferenciales 📖
+
+<hr>
+
+En estas prácticas veremos como resolver ecuaciones diferenciales utilizando **sympy**,
+en nuestro caso utilizaremos la siguiente ecuación:
+
+[![Captura-de-pantalla-de-2021-05-10-11-29-34.png](https://i.postimg.cc/rsg0D5Sr/Captura-de-pantalla-de-2021-05-10-11-29-34.png)](https://postimg.cc/V0rLTrZf)
+
+La ecuación representa el crecimiento poblacional, el cual se explico paso a paso en la clase y ademas se encuentra documentado
+explicando esos pasos.
--- sympy/practice_sympy.py
+++ sympy/practice_sympy.py
@@ -1,19 +1,26 @@
 import sympy as s
 
 if __name__ == '__main__':
+    # Se asignan los símbolos y la función
     alpha, beta, t = s.symbols("alpha beta t")
     C1, t0 = s.symbols("C1 t0")
     f = s.Function('f')
 
+    # Se deriva la función con respecto a t
     dfdt = s.diff(f(t), t)
+
+    # Se define la ecuación a resolver
     eq = s.Eq(dfdt, alpha * f(t) + beta * f(t) ** 2)
     solution = s.dsolve(eq)
 
+    # Se obtiene el lado derecho, que nos permite determinar el valor del C1
+    # al aplicar las condiciones de contorno
     sol_g = solution.rhs
 
     # Se aplican las condiciones de contorno
     sol_t0 = sol_g.subs(t, 0)
 
+    # Se obtiene C1
     condition_ini = s.Eq(sol_t0, t0)
     solutions = s.solve(condition_ini, C1)
     value_C1 = solutions[0]
@@ -23,9 +30,8 @@ if __name__ == '__main__':
 
     # La simplificación es:
     solution_part_simplify = s.simplify(sol_part)
-
     print(f"La solución particular simplificada es: {solution_part_simplify}")
 
-    # Algunas de las verificaciones son:
+    # Se puede verificar con la siguiente linea:
     print(f"Cuando t=0: {solution_part_simplify.subs(t, 0)}")
 
